482d39d28182297cb90fdb3f5762700c08f0de82
moniK1223 <160087438+moniK1223@users.noreply.github.com>
2024-04-27 15:33:41 +0530

    Add files via upload

---
+++ assert_keyword.py
@@ -0,0 +1,70 @@
+# num = int(input('Enter the number : '))
+#
+# if num % 2 ==0:
+#     print('even')
+# else:
+#     raise Exception
+
+#----------------------------------------
+'''
+assert : assert is a keyword used for validating the condition
+assert will take condition as a parameter, if the condition is True, it will continue the operations,
+if the condition is False, assert will always give AssertionError
+
+SYNTAX : assert <condition>
+
+'''
+
+#------------------------------------------------------
+# num = int(input('Enter the number : '))
+#
+# assert num%2==0     ## num = 20
+# print('hello world')
+#
+# ## op --> hello world
+
+
+#---------------------------------------------------
+num = int(input('Enter the number : '))
+
+assert num%2==0     ## num = 21
+print('hello world')
+
+## op --> AssertionError
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
